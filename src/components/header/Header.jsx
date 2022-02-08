e7f14ad3fe78e671b0329505eb4baf4394a43a2c
Nathalie Bruinhof <nmbruinhof@gmail.com>
2022-02-08 16:59:23 +0100

    Made menu working

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,19 @@
+import React, { useState } from 'react';
 import { Navigation } from '../navigation/';
 import * as Styled from './Header.styled.js';
 import Hamburger from "../../../assets/icons/Hamburger.svg"
 
 export const Header = () => {
+    const [isActive, setIsActive] = useState(false);
+    const handleClick = () => setIsActive(!isActive);
     return (
         <Styled.Header>
             <Styled.Wrapper>
               <a href="#">team awesome</a>
-             <Hamburger />
+             <Hamburger onClick={handleClick}/>
           </Styled.Wrapper>
-        <Navigation />
+          {isActive && (
+        <Navigation />)}
       </Styled.Header>
     );
   }
