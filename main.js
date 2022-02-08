00fb23c29501204ecb90c9daff19ccbff89d7c30
Nathalie Bruinhof <nmbruinhof@gmail.com>
2022-02-08 12:01:30 +0100

    Desktop responsiveness

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const toggleButton = document.querySelector(".navigation");
+const toggleButton = document.querySelector(".header__navigation");
 const navLink = document.querySelector(".nav__link")
 toggleButton.addEventListener("click", () => {
     navLink.classList.toggle("hide");
